fe62abf0fb97ac1b985df1e33f2f2630db868a8f
Jake <111051681+JakobCoding@users.noreply.github.com>
2023-03-03 19:40:16 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = () => {
+  const name = 'Jane!';
+  
+
+  return (
+    <div className="App">
+     <h1>Hello, {name}</h1>
+    </div>
+  );
+}
+
+export default App;
